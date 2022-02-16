e709e9da0127912e24b7daeef7f3605f632ae664
Enes Çetintaş <enesctnts@gmail.com>
2022-02-16 14:56:36 +0300

    16.02.2022

---
+++ NesneGenel/Abstract/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+         abstract class Sekil
+        {
+            public string Renk  { get; set; }
+        }
+        class Daire:Sekil
+        {
+
+        }
+        class Silindir:Daire
+        {
+
+        }
+        sealed class Koni:Silindir
+        {
+
+        }
+        //class KesikKoni : Koni { } ==> Koniyi sealed tanımladıgımız için koniyi miras veremiyoz
+
+        static class Veritabani
+        {
+            //public  void Kaydet1() { } // Hatalı Static classların propertyleride static olmalıdır
+            public static void Kaydet() { }
+        }
+
+        class Asker
+        {
+            static int sayac = 0;
+            public Asker()
+            {
+                sayac++;
+                Console.WriteLine(sayac);
+            }
+            
+            ~Asker()
+            {
+                sayac--;
+                Console.WriteLine(sayac);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //Abstract oldugu içi olmaz sadece başka yerden biras alırsa olur.
+            //Sekil sekil = new Sekil();
+
+            Asker asker1 = new Asker();
+            Asker asker2 = new Asker();
+            Asker asker3 = new Asker();
+
+        }
+    }
+}
---
+++ NesneGenel/NesneGenel/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerslerGenel
+{
+
+    class Asker
+    {
+        public int SicilNo { get; set; }
+        public string Vazife { get; set; }
+        protected void islem() // sadece tanımlı oldugu class ta ve ondan türeyen classlarda gözükür.
+        {
+
+        }
+    }
+
+    class Er : Asker
+    {
+
+    }
+
+
+    class Daire
+    {
+        public double r { get; set; }
+        public double DaireAlanHesapla()
+        {
+            return Math.PI * r;
+        }
+    }
+
+
+    class Silindir : Daire
+    {
+        public int h { get; set; }
+
+        public double SilindirHacimHesapla()
+        {
+            return base.DaireAlanHesapla() * h;
+        }
+    }
+
+
+    class Koni : Silindir
+    {
+
+        public double KoniHacimHesapla()
+        {
+            return base.SilindirHacimHesapla() / 3;
+        }
+    }
+
+    class Depo
+    {
+        private string[] _depo = new string[10];
+
+        public string this[int indis]
+        {
+            get
+            {
+                return _depo[indis];
+            }
+            set
+            {
+                _depo[indis] = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Inheritance(Miras,Kalıtım,Türetme)
+            //Asker asker = new Asker();
+            //Er er = new Er();
+
+
+            //Daire daire = new Daire() { r = 5 };
+            //Console.WriteLine("Daire Sonuç: " + daire.DaireAlanHesapla());
+
+            //Silindir silindir = new Silindir() { h = 10, r = 3 };
+            //Console.WriteLine("Silindir Sonuç: " + silindir.SilindirHacimHesapla());
+
+            //Koni koni = new Koni() { h = 10, r = 3 };
+            //Console.WriteLine("Koni Sonuç: " + koni.KoniHacimHesapla());
+
+
+            //Depo depo = new Depo();
+            //depo[0] = "";
+
+
+
+        }
+    }
+}
+
